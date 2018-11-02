1b70ff54bf8d8de59f28266eb128d77309a12e61
AA047267 <37131020+AA047267@users.noreply.github.com>
2018-11-02 20:57:43 +0530

    Variable file

---
+++ elb-asg/variables.tf
@@ -0,0 +1,12 @@
+variable "aws-region" {
+	default = "ap-south-1"
+}
+
+variable "ami" {
+	default = "ami-0ed73e88d091d983d"   #AMI created from a web server
+}
+
+variable "count" {
+	default = 1
+}
+
